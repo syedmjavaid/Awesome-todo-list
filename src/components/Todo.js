395e3a5ada68bf816123b851cc3b1ea581378c15
syedmjavaid <syedmjavaid@gmail.com>
2022-02-01 23:26:17 +0000

    search bar added and reset search bar entry

---
+++ src/components/Todo.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
+function Todo() {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+
+  return TodoList.map((todo, index) => (
+    <div
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
+      key={index}
+    ></div>
+  ));
+}
+
+export default Todo;
