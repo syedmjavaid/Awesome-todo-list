167e5dddc106fe06d35b0e6cb7a306153fef73f0
syedmjavaid <syedmjavaid@gmail.com>
2022-02-02 00:42:55 +0000

    todo list accepting entries with icons added but doesnt render

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
 
-function Todo() {
+function Todo({ todos, completeTodo, removeTodo }) {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
@@ -12,8 +14,24 @@ function Todo() {
     <div
       className={todo.isComplete ? "todo-row complete" : "todo-row"}
       key={index}
-    ></div>
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className="icons">
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className="delete-icon"
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className="edit-icon"
+        />
+      </div>
+    </div>
   ));
 }
 
 export default Todo;
+
+// 32.00
